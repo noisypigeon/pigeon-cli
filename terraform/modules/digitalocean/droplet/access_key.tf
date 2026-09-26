0f3452d5369f5b0118125c25d570df4788e2f144
noisypigeon <167243387+noisypigeon@users.noreply.github.com>
2026-09-26 00:29:22 -0700

    fix: point droplet module's access-key dependency at pigeon, not pigeon-tf

--- terraform/modules/digitalocean/droplet/access_key.tf
+++ terraform/modules/digitalocean/droplet/access_key.tf
@@ -1,5 +1,5 @@
 module "compute_bucket_access_key" {
-  source = "git::https://github.com/noisypigeon/pigeon-tf.git//digitalocean/access-key?ref=digitalocean/access-key/v0.1.0"
+  source = "git::https://github.com/noisypigeon/pigeon.git//terraform/modules/digitalocean/access-key?ref=terraform/modules/digitalocean/access-key/v0.1.0"
   name   = "${var.namespace}-${random_string.suffix.result}-${var.name}"
 
   bucket_grants = local.bucket_names
